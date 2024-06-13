e82561cac9d73b1697a6686239b8f9582749807d
Hayden <hayden.dev.k@gmail.com>
2024-06-13 17:28:23 +0900

    Fix: 깃허브 id 중복 이슈 처리

--- src/auth.ts
+++ src/auth.ts
@@ -51,27 +51,56 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     // 크리덴셜도 깃허브도 성공하면 여기로 가는데, 크리덴셜에 있는 값이 깃허브에는 없다!
     signIn: async ({ user, account }: { user: any; account: any }) => {
       console.log("signIn", user, account); // 여기엔 크리덴셜의 user.id, user.role 값 존재하므로 아래의 jwt에서 드리블해주도록 한다
+      // if (account?.provider === "github") {
+      //   const { id, email } = user; // 깃허브가 주는 유저 id로 몽고디비에서 존재하는지 조회 (몽고디비 기준 authProviderId 값으로 조회)
+      //   await connectDB();
+      //   const existingUser = await User.findOne({ authProviderId: "github" });
+      //   if (!existingUser) {
+      //     // 없으면 DB에 추가
+      //     await new User({
+      //       name: user.name,
+      //       email: user.email,
+      //       authProviderId: "github",
+      //       role: "user",
+      //     }).save();
+      //   }
+
+      //   // 있으면 정보 가져오기
+      //   const githubUser = await User.findOne({ authProviderId: "github" });
+      //   user.role = githubUser.role || "user"; // 깃허브에 없는 타입이라
+      //   user.id = githubUser._id || null;
+      //   return true; // 깃허브 로그인 통과!
+      // } else {
+      //   return true; // 크레덴셜 로그인 통과!
+      // }
+
+      // github id 중복 이슈
       if (account?.provider === "github") {
-        const { id, email } = user; // 깃허브가 주는 유저 id로 몽고디비에서 존재하는지 조회 (몽고디비 기준 authProviderId 값으로 조회)
-        await connectDB();
-        const existingUser = await User.findOne({ authProviderId: user.id });
+        const { name, email } = user;
+        await connectDB(); // mongodb 연결
+        const existingUser = await User.findOne({
+          email,
+          authProviderId: "github",
+        });
         if (!existingUser) {
-          // 없으면 DB에 추가
+          // 소셜 가입
           await new User({
-            name: user.name,
-            email: user.email,
-            authProviderId: user.id,
+            name,
+            email,
+            authProviderId: "github",
             role: "user",
           }).save();
         }
-
-        // 있으면 정보 가져오기
-        const githubUser = await User.findOne({ authProviderId: user.id });
-        user.role = githubUser.role || "user"; // 깃허브에 없는 타입이라
-        user.id = githubUser._id || null;
-        return true; // 깃허브 로그인 통과!
+        const socialUser = await User.findOne({
+          email,
+          authProviderId: "github",
+        });
+        user.role = socialUser?.role || "user";
+        user.id = socialUser?._id || null;
+        return true;
       } else {
-        return true; // 크레덴셜 로그인 통과!
+        // 크레덴셜 통과
+        return true;
       }
     },
     async jwt({ token, user }: { token: any; user: any }) {
